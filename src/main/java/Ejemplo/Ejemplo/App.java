619ad11150a2fe92a6b6b138af1e7316a4829db9
Antonella Palilla <APalilla@teco.com.ar>
2022-06-25 01:00:26 -0300

    se agrego a la clase EJ el afterTest driver.close

---
+++ src/main/java/Ejemplo/Ejemplo/App.java
@@ -0,0 +1,13 @@
+package Ejemplo.Ejemplo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
